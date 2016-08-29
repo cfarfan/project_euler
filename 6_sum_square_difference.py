36c5332db84179aea69ba29c3ee04a8486052b9a
Camilo Farfán <camilo.farfan@inzpiral.com>
2016-08-29 11:54:08 -0300

    Problem 6: Python: Sum square difference

---
+++ 6_sum_square_difference.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# The sum of the squares of the first ten natural numbers is,
+
+# 12^2 + 22^2 + ... + 102^2 = 385
+# The square of the sum of the first ten natural numbers is,
+
+# (1 + 2 + ... + 10)^2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural
+# numbers and the square of the sum is 3025 − 385 = 2640
+
+# Find the difference between the sum of the squares of the first one hundred
+# natural numbers and the square of the sum.
+
+n = 100
+
+def sumSquareDifference(num):
+  sum_sqr = 0
+  sqr_sum = 0
+  for i in xrange(1,num+1):
+    sum_sqr += pow(i,2)
+    sqr_sum += i
+  sqr_sum = pow(sqr_sum,2)
+  return sqr_sum-sum_sqr
+
+print sumSquareDifference(n)
